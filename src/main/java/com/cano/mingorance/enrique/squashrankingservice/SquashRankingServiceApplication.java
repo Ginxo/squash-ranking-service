32bc0f9478bc76ae8aeb214566bf003c972de36f
Enrique Mingorance Cano <ginxo@hotmail.com>
2018-07-11 15:10:46 +0200

    spring boot starter creation

---
+++ src/main/java/com/cano/mingorance/enrique/squashrankingservice/SquashRankingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cano.mingorance.enrique.squashrankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquashRankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquashRankingServiceApplication.class, args);
+	}
+}
